387f22b4edf8bd1db8f94ba5fe576431a776cf56
evgenyodessa <evheniistoianov@gmail.com>
2022-09-26 21:37:44 +0300

    7.5 redone

---
+++ dz8/dz 8.2.py
@@ -0,0 +1,14 @@
+# Напишите программу где пользователь вводит число - count, а программа выводит count чисел фибоначчи.
+count=int(input("vvod"))
+a=0
+print(a)
+b=1
+print(b)
+
+for i in range(count):
+    b=a+b
+    a=b-a
+    print(b)
+
+
+
