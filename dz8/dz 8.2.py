de6f00dfb25b45d98450d56a0bf7450e0775567e
evgenyodessa <evheniistoianov@gmail.com>
2022-10-01 00:12:00 +0300

    8.2 redone

--- dz8/dz 8.2.py
+++ dz8/dz 8.2.py
@@ -1,5 +1,8 @@
 # Напишите программу где пользователь вводит число - count, а программа выводит count чисел фибоначчи.
-count = int(input("vvod"))
+count = int(input("Enter amount of Fibonacci's' numbers: "))
+if count == 1:
+    print(0)
+    exit()
 a = 0
 print(a)
 b = 1
