29e14fdc2e2d818eb1770e6059292efdbb18de65
evgenyodessa <evheniistoianov@gmail.com>
2022-10-08 03:29:51 +0300

    10.2 redone

--- dz10/10.2.py
+++ dz10/10.2.py
@@ -3,51 +3,32 @@
 # Необходимо учитывать то, что есть разные выигрышные варианты и программа должна их распознавать.
 
 A = [
-    ['x', 'x', 'o'],
-    ['o', 'o', 'x'],
-    ['x', 'o', 'o']
+    ['x', 'x', 'x'],
+    ['x', 'o', 'o'],
+    ['o', 'o', 'x']
 ]
-if True:
-    for i, i_value in enumerate(A):
-        a = 0
-        for j, j_value in enumerate(i_value):
-            if str(A[i][j]) == 'x':
-                a += 1
-            elif str(A[i][j]) == 'o':
-                a -= 1
-            if a == 3:
-                print("x победил")
-                exit()
-            elif a == -3:
-                print("o победил")
-                exit()
-
-    for i, i_value in enumerate(A):
-        a = 0
-        for j, j_value in enumerate(i_value):
-            if str(A[j][i]) == 'x':
-                a += 1
-            elif str(A[j][i]) == 'o':
-                a -= 1
-            if a == 3:
-                print("x победил")
-                exit()
-            elif a == -3:
-                print("o победил")
-                exit()
+c = 0
+for i, i_value in enumerate(A):
     a = 0
-    for i, i_value in enumerate(A):
-        for j, j_value in enumerate(i_value):
-            if str(A[i][j]) == str(A[j][i]) == 'x':
-                a += 1
-            elif str(A[i][j]) == str(A[j][i]) == 'o':
-                a -= 1
-            if a == 3:
-                print("x победил")
-                exit()
-            elif a == -3:
-                print("o победил")
-                exit()
-    else:
-        print("Ничья")
-
+    b = 0
+    for j, j_value in enumerate(i_value):
+        if str(A[i][j]) == 'x':
+            a += 1
+        elif str(A[i][j]) == 'o':
+            a -= 1
+        if str(A[j][i]) == 'x':
+            b += 1
+        elif str(A[j][i]) == 'o':
+            b -= 1
+        if str(A[0][0]) == str(A[1][1]) == str(A[2][2]) == 'x' or str(A[0][2]) == str(A[2][0]) == str(A[1][1]) == 'x':
+            c += 1
+        if str(A[0][0]) == str(A[1][1]) == str(A[2][2]) == 'o' or str(A[0][2]) == str(A[2][0]) == str(A[1][1]) == 'o':
+            c -= 1
+        if a == 3 or b == 3 or c == 3:
+            print("x win")
+            exit()
+        elif a == -3 or b == -3 or c == -3:
+            print("o win")
+            exit()
+else:
+    print("Ничья")
