3360837f556f9985e4c89aa66bb717dd87defd4e
evgenyodessa <evheniistoianov@gmail.com>
2022-10-13 19:34:39 +0300

    11.2 done

---
+++ dz11/11.2.py
@@ -0,0 +1,16 @@
+n = int(input('enter amount of files: '))
+keys = {'write': 'W', 'read': 'R', 'execute': 'X'}
+A = {}
+for i in range(n):
+    a = input('enter file name: ')
+    b = input('enter permission for file: ')
+    A[a] = set(b)
+print(A)
+m = int(input('enter amount of files for checking permissions: '))
+for i in range(m):
+    c = input('enter permission for file: ')
+    d = input('enter file name: ')
+    if keys[c] in A[d]:
+        print('ok')
+    else:
+        print('access denied')
