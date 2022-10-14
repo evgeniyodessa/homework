136f6a21afe95b83696cb3647ee8a8595c313c2f
evgenyodessa <evheniistoianov@gmail.com>
2022-10-14 15:42:52 +0300

    11.2 redone

--- dz11/11.2.py
+++ dz11/11.2.py
@@ -2,14 +2,11 @@ n = int(input('enter amount of files: '))
 keys = {'write': 'W', 'read': 'R', 'execute': 'X'}
 A = {}
 for i in range(n):
-    a = input('enter file name: ')
-    b = input('enter permission for file: ')
+    a, b = input('enter file name and permission for file: ').split()
     A[a] = b
-
 m = int(input('enter amount of files for checking permissions: '))
 for i in range(m):
-    c = input('enter permission for file: ')
-    d = input('enter file name: ')
+    c, d = input('enter permission for file and file name: ').split()
     if keys[c] in A[d]:
         print('ok')
     else:
